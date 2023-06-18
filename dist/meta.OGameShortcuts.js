3f91c5f75fbf817a99af3f35ede4c323c5b0d8e2
Ivan Ramírez <ivan_amigue@hotmail.com>
2023-06-18 14:23:13 -0600

    Update meta.OGameShortcuts.js
    Actualización de versión

--- dist/meta.OGameShortcuts.js
+++ dist/meta.OGameShortcuts.js
@@ -2,7 +2,7 @@
 // @name           OGame Redesign: Keyboard Shortcuts
 // @description    Assigns keyboard shortcuts to various game functions | Fixes for v8.7.0
 // @namespace      Vesselin | Modified by JBWKZ2099
-// @version        3.2.1
+// @version        3.2.2
 // @date           2012-12-14
 // @author         Vesselin Bontchev
 // @include        *://*.ogame.gameforge.com/game/index.php?*
